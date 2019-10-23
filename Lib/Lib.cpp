c5adf95a58640d0ae0b300d70a325ca2f07ea7d6
Swagat <swagat.bhattacharyya@gmail.com>
2019-10-23 19:26:20 -0400

    Made the library constructor

--- Lib/Lib.cpp
+++ Lib/Lib.cpp
@@ -1,1 +1,9 @@
 // These are the function declarations in the Headset library
+#include "Lib.h"
+
+//////////////// Library Constructor ////////////////////////
+// This handles the initialization and setup of the library
+Lib::Lib(void) {
+	// Blank...
+}
+/////////////////////////////////////////////////////////////
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Lib.h    :: This contains function declarations and the global pinmap
 Lib.cpp  :: This contains the meat of the functions/modules used in Main.ino
 
 Functions:
-Blink      :: 
+Blink      :: This function flashes the LED for a given amount of time
 Tone       ::
 ReadDist   ::
 FiltUpdate ::
